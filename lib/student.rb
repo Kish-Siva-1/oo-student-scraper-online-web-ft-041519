7be104e4ac2da5c55981b8d520c9a6e0756c565c
Kish Siva <rodes88@msn.com>
2019-05-13 16:45:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    @@all = students_array
+    students.array.each do |x|
+      self.new(x)
+    end
   end
 
   def add_student_attributes(attributes_hash)
