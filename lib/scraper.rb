e88e548394b13e82291ffe8e32ddc0985a2d8ea3
Kish Siva <rodes88@msn.com>
2019-05-13 15:55:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -46,6 +46,11 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     profile_page = Nokogiri::HTML(open(profile_url))
     
+    tweet = 0
+    linkedin = 0
+    github = 0
+    blog = 0 
+    
     profile_page.css('.social-icon-container a').each do |x| 
     binding.pry
       if x.attr('href').include?("twitter")
