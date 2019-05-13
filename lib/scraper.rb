c4641503f9ced9e0a3bb7b35ccf4b76cc87f5c97
Kish Siva <rodes88@msn.com>
2019-05-13 15:30:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -53,7 +53,7 @@ class Scraper
      :profile_quote => profile_page.css('.profile-quote').text,
      :bio => profile_page.css(".description-holder p").text
     }
-    binding.pry
+    prof_link
     #profile_page.css(".profile-photo a").each do |profile_link|
     #    st_profile_url = "#{profile_link.attr('href')}"
     #    profie_scrape << {name: st_name, location: st_location, profile_url: st_profile_url}
