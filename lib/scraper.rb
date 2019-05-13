ea05a8ab55b0f3226e3c390a86e81db3cd7a7636
Kish Siva <rodes88@msn.com>
2019-05-13 15:23:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -46,11 +46,11 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     prof_link = []
     profile_page = Nokogiri::HTML(open(profile_url))
-    prof_link = {:twitter => profile_page.css('.social-icon-container a')[0].attr('href')
-     :linkedin => profile_page.css('.social-icon-container a')[1].attr('href')
-     :github => profile_page.css('.social-icon-container a')[2].attr('href')
-     :blog => profile_page.css('.social-icon-container a')[3].attr('href')
-     :profile_quote => profile_page.css('.profile-quote').text
+    prof_link = {:twitter => profile_page.css('.social-icon-container a')[0].attr('href'),
+     :linkedin => profile_page.css('.social-icon-container a')[1].attr('href'),
+     :github => profile_page.css('.social-icon-container a')[2].attr('href'),
+     :blog => profile_page.css('.social-icon-container a')[3].attr('href'),
+     :profile_quote => profile_page.css('.profile-quote').text,
      :bio => profile_page.css(".description-holder p").text
     }
     #profile_page.css(".profile-photo a").each do |profile_link|
