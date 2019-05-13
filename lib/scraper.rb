18f25d5ff663a93c083dbe156517e5e30b01c9e1
Kish Siva <rodes88@msn.com>
2019-05-13 16:12:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,32 +18,9 @@ class Scraper
   scraped_students
 
   end
-
-  #describe "#scrape_profile_page" do
-  #  it "is a class method that scrapes a student's profile page and returns a hash of attributes describing an individual #student" do
-  #    profile_url = "./fixtures/student-site/students/joe-burgess.html"
-  #    scraped_student = Scraper.scrape_profile_page(profile_url)
-  #    expect(scraped_student).to be_a(Hash)
-  #    expect(scraped_student).to match(student_joe_hash)
-  #  end
-
-  #  it "can handle profile pages without all of the social links" do
-  #    profile_url = "./fixtures/student-site/students/david-kim.html"
-  #    scraped_student = Scraper.scrape_profile_page(profile_url)
-  #    expect(scraped_student).to be_a(Hash)
-  #    expect(scraped_student).to match(student_david_hash)
-  #  end
-  #end
-
-  #let!(:student_joe_hash) {{:twitter=>"https://twitter.com/jmburges",
-  #                          :linkedin=>"https://www.linkedin.com/in/jmburges",
-  #                          :github=>"https://github.com/jmburges",
-  #                          :blog=>"http://joemburgess.com/",
-  #                          :profile_quote=>"\"Reduce to a previously solved problem\"",
-  #                          :bio=>
-  #"I grew up outside of the Washington DC (NoVA!) and went to college at Carnegie Mellon University in Pittsburgh. After college, I worked as an Oracle consultant for IBM for a bit and now I teach here at The Flatiron School."}}
-
+  
   def self.scrape_profile_page(profile_url)
+    
     profile_page = Nokogiri::HTML(open(profile_url))
     
     prof_link = {}
@@ -77,19 +54,6 @@ class Scraper
     prof_link[:profile_quote] = profile_page.css('.profile-quote').text
     prof_link[:bio] = profile_page.css(".description-holder p").text
     
-    #prof_link = {:twitter => if tweet then tweet end,
-    # :linkedin => if linkedin then linkedin end,
-    # :github => if github then github end,
-    # :blog => if blog then blog end,
-    # :profile_quote => profile_page.css('.profile-quote').text,
-    # :bio => profile_page.css(".description-holder p").text
-    #}
-    binding.pry
-    prof_link
-    #profile_page.css(".profile-photo a").each do |profile_link|
-    #    st_profile_url = "#{profile_link.attr('href')}"
-    #    profie_scrape << {name: st_name, location: st_location, profile_url: st_profile_url}
-    #  end
   end
 
 end
