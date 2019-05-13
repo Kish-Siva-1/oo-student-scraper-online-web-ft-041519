68b7fd14405f835a1af461259e80a3b298fdb889
Kish Siva <rodes88@msn.com>
2019-05-13 16:25:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,7 @@ class Student
   def initialize(student_hash)
     @name = student_hash[:name] if student_hash[:name] 
     @location = student_hash[:location] if student_hash[:location]
+    @@all << self 
   end
 
   def self.create_from_collection(students_array)
