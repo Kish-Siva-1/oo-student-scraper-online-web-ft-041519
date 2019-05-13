761520582a503b8ba1c1cd7188622e1383d7fb44
Kish Siva <rodes88@msn.com>
2019-05-13 15:27:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -47,10 +47,10 @@ class Scraper
     prof_link = []
     profile_page = Nokogiri::HTML(open(profile_url))
     binding.pry
-    #prof_link = {:twitter => profile_page.css('.social-icon-container a')[0].attr('href').text,
+    #prof_link = {:twitter => profile_page.css('.social-icon-container a')[0].attr('href'),
     # :linkedin => profile_page.css('.social-icon-container a')[1].attr('href'),
-    # :github => profile_page.css('.social-icon-container a')[2].attr('href').text,
-    # :blog => profile_page.css('.social-icon-container a')[3].attr('href').text,
+    # :github => profile_page.css('.social-icon-container a')[2].attr('href'),
+    # :blog => profile_page.css('.social-icon-container a')[3].attr('href'),
     # :profile_quote => profile_page.css('.profile-quote').text,
     # :bio => profile_page.css(".description-holder p").text
     #}
